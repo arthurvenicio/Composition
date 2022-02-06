320ece7d30bcfba549afba789e63bd082d151d77
arthurvenicio <stuks.midia@gmail.com>
2022-02-06 00:21:07 -0300

    Create class's

---
+++ Composition/Entities/HourContract.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Composition.Entities
+{
+    internal class HourContract
+    {
+        public DateTime Date { get; set; }
+        public double ValuePerHour { get; set; }
+        public int Hours { get; set; }
+
+        public HourContract() { }
+        public HourContract(DateTime date, double valuePerHour, int hours) 
+        {
+            this.Date = date;
+            this.ValuePerHour = valuePerHour;
+            this.Hours = hours;
+        }
+
+        public double totalValue()
+        {
+            return this.ValuePerHour * this.Hours;
+        }
+    }
+}
---
+++ Composition/Entities/Worker.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using Composition.Entities.Enums;
+
+namespace Composition.Entities
+{
+    internal class Worker
+    {
+        public string Name { get; set; }
+        public WorkerLevel Level { get; set; }
+
+        public double BaseSalary { get; set; }
+
+        public Worker() { }
+        public Worker(string name, WorkerLevel level, double baseSalary) 
+        {
+            this.Name = name;
+            this.Level = level;
+            this.BaseSalary = baseSalary;
+        }
+
+    }
+}
