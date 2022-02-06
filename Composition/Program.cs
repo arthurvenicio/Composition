73781d996e2d4ee192d337a26795dfa59679d2a2
arthur.souza <arthur.souza@CTNDELLCHZVJB3>
2022-02-06 00:04:50 -0300

    Add project files.

---
+++ Composition/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Composition
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
